9d775db3eb4c108307efebc9b4e21c8f4bb29458
pascwin <winner@hm.edu>
2020-05-22 16:49:32 +0200

    shiat

--- background-generator/script.js
+++ background-generator/script.js
@@ -1,3 +1,7 @@
+import { without } from "lodash";
+
+console.log(without)
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
@@ -5,12 +9,12 @@ var body = document.getElementById("gradient")
 
 function setGradient() {
     body.style.background =
-    "linear-gradient(to right, "
-    + color1.value
-    + ", " 
-    + color2.value
-    + ")";
-    
+        "linear-gradient(to right, "
+        + color1.value
+        + ", "
+        + color2.value
+        + ")";
+
     css.textContent = body.style.background + ";"
 }
 
